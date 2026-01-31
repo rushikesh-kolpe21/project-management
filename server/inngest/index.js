a270713119fbc8bcdc45bb4b33b84038ef66c781
Rushikesh kolpe <155113038+rushikesh-kolpe21@users.noreply.github.com>
2026-01-31 19:50:48 +0530

    first commit

---
+++ server/inngest/index.js
@@ -0,0 +1,66 @@
+
+const { Prisma } = require("@prisma/client");
+const { Inngest } = require("inngest");
+
+// Create a client to send and receive events
+const inngest = new Inngest({ id: "project-management" });
+
+// Inngest functions to SAVE USER DATA to a database, send notifications, etc.
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+    async ({event})=>{
+        const {data} = event
+        await Prisma.user.create({
+            data:{
+                id: data.id,
+                email: data?.email_addresses[0]?.email_addres,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            }
+        })
+    }
+)
+
+// Inngest functions to DELTE USER DATA to a database,
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-from-clerk'},
+    {event: 'clerk/user.deleted'},
+    async ({event})=>{
+        const {data} = event
+        await Prisma.user.delete({
+            where:{
+                id: data.id,
+                
+            }
+        })
+    }
+)
+
+// Inngest functions to UPDATE USER DATA to a database, send notifications, etc.
+const syncUserUpdation = inngest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+    async ({event})=>{
+        const {data} = event
+        await Prisma.user.update({
+            where:{
+                id:data.id,
+            },
+            data:{
+                email: data?.email_addresses[0]?.email_addres,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            }
+        })
+    }
+)
+
+// Create an empty array where we'll export future Inngest functions
+const functions = [
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation, 
+];
+
+module.exports = { inngest, functions };   
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const expresss = require('express');
+
+
+const { serve } = require("inngest/express");
+const { inngest, functions } = require("./inngest/index.js");
+
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+
+
+const {clerkMiddleware} = require("@clerk/express");
+
+
+const app = expresss();
+app.use(expresss.json());
+app.use(cors());
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(clerkMiddleware())
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
